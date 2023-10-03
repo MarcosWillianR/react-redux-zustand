265666a93a231b20d6deb24deb0dea3f035577b9
Marcos Willian <markusuuuu@gmail.com>
2023-10-02 22:56:24 -0300

    feat: added Radix Collapsible on modules

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -25,6 +25,9 @@ export function Player() {
 
           <aside className="w-80 divide-y-2 divide-zinc-900 scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800 absolute top-0 bottom-0 right-0 overflow-y-auto border-l border-zinc-800 bg-zinc-900">
             <Module moduleIndex={0} title="Desvendando o Redux" lessonsAmount={1} />
+            <Module moduleIndex={0} title="Desvendando o Redux" lessonsAmount={1} />
+            <Module moduleIndex={0} title="Desvendando o Redux" lessonsAmount={1} />
+            <Module moduleIndex={0} title="Desvendando o Redux" lessonsAmount={1} />
           </aside>
         </main>
       </div>
