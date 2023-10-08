2bfa09491d464ec5b2262127f1d2919e421a150c
Marcos Willian <markusuuuu@gmail.com>
2023-10-08 14:02:56 -0300

    feat: created global hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,12 @@
-import { useAppSelector } from "../store"
+import { useCurrentLesson } from "../store/slices/player"
 
 export function Header() {
-  const { currentLessonTitle, currentModuleTitle } = useAppSelector(state => {
-    const { currentLessonIndex, currentModuleIndex } = state.player
-
-    const currentModuleTitle = state.player.course.modules[currentModuleIndex].title
-    const currentLessonTitle = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex].title
-
-    return { currentModuleTitle, currentLessonTitle }
-  })
+  const { currentLesson, currentModule } = useCurrentLesson();
   
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">{currentLessonTitle}</h1>
-      <span className="text-sm text-zinc-400">Módulo "{currentModuleTitle}"</span>
+      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
+      <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
     </div>
   )
 }
